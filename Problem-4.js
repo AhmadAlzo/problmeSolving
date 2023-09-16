39be16df4f0df17b9b8aabc495d427b5dabdb2bd
AhmadAlzo <124163736+AhmadAlzo@users.noreply.github.com>
2023-09-16 09:07:06 +0300

    Update Problem-4.js

--- Problem-4.js
+++ Problem-4.js
@@ -13,7 +13,9 @@ function check(e){
 
     a.split("").forEach((e,i)=>{
      	if(close.includes(e)){
-            if(obj[Open[close.indexOf(e)]]>obj[e]){obj[e]++}
+            if(obj[Open[close.indexOf(e)]]>obj[e]){
+                obj[e]++
+            }else obj[e]--
         }else if(a[i+1]==close[Open.indexOf(e)]||Open.includes(a[i+1])||i==a.length-1){
         	obj[e]++
         }
@@ -22,4 +24,3 @@ function check(e){
     	return true
     }else return false
 }
-
